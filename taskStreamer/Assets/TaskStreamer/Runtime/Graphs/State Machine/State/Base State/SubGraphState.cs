73c13aabd0ce2c5cf87bf5d5f71a8a07644d0246
Stellar-F0X <jms030907@gmail.com>
2025-09-27 14:32:25 +0900

    feat: Sub BT가 실행 중에 전이 옵션 구현
    FSM에서 Sub Behavior Tree가 실행 중이라도, Transition이 가능하도록 옵션을 추가함.

--- taskStreamer/Assets/TaskStreamer/Runtime/Graphs/State Machine/State/Base State/SubGraphState.cs
+++ taskStreamer/Assets/TaskStreamer/Runtime/Graphs/State Machine/State/Base State/SubGraphState.cs
@@ -6,50 +6,55 @@ using UnityEngine;
 namespace TaskStreamer.FSM
 {
     /// <summary>
-    /// Represents a specialized type of state node in a finite state machine (FSM)
-    /// that encapsulates the functionality of a subgraph.
-    /// This abstract base class is designed specifically for handling operations
-    /// related to subgraphs within a broader graph structure.
+    /// 유한 상태 머신(FSM)의 상태 노드 중 하나로, 특정 서브그래프의 동작을 캡슐화하는 추상 클래스.
+    /// 서브그래프와 관련된 작업을 처리하기 위한 기능을 제공하며, 그래프 구조 안에서 서브그래프의 역할을 정의한다.
     /// </summary>
     /// <remarks>
-    /// Each instance of <see cref="SubGraphState"/> corresponds to an individual subgraph,
-    /// defined by a unique identifier. Subclasses of this class are required to implement
-    /// the behavior and type of the subgraph being used.
+    /// <see cref="SubGraphState"/> 클래스의 각 인스턴스는 고유 식별자로 정의된 개별 서브그래프를 나타낸다.
+    /// 서브클래스는 사용하려는 서브그래프의 구체적인 동작과 유형을 반드시 구현해야 한다.
     /// </remarks>
-    [Serializable]
+    [Serializable, GeneratePropertyBag, Readable]
     internal abstract class SubGraphState : StateBase, ISubGraphProvider
     {
         /// <summary>
-        /// Represents a private instance of a subgraph associated with the current state.
+        /// 특정 상태가 실행 중(execution)에도 전이(transition)가 가능한지를 나타내는 변수입니다.
         /// </summary>
         /// <remarks>
-        /// This variable holds the reference to a subgraph that belongs to the state.
-        /// It is initialized during the state awakening process and is critical for
-        /// managing the subgraph's lifecycle, including resetting and updating its state
-        /// during transitions and updates in the finite state machine.
+        /// 이 변수는 상태 실행 도중 전이 허용 여부를 정의하며, <see cref="BlackboardVariable{TValue}"/>로 구현되어
+        /// 런타임 중 동적으로 값을 관리할 수 있습니다.
+        /// </remarks>
+        public BlackboardVariable<bool> canTransitionDuringExecution;
+
+
+        /// <summary>
+        /// 현재 상태와 연관된 서브그래프를 나타내는 비공개 인스턴스입니다.
+        /// </summary>
+        /// <remarks>
+        /// 이 변수는 상태와 속한 서브그래프에 대한 참조를 저장합니다.
+        /// 상태가 초기화될 때 초기화되며, 상태 전이 및 상태 업데이트 중 서브그래프의 상태를 재설정하거나 업데이트하는 데 중요합니다.
+        /// 서브그래프의 생명주기를 관리하는 역할을 수행합니다.
         /// </remarks>
         [DontCreateProperty]
         private Graph _subGraph;
 
         /// <summary>
-        /// Represents the unique identifier (GUID) of a sub-graph associated with this state.
+        /// 해당 상태와 연관된 서브 그래프의 고유 식별자(GUID)를 나타내는 비공개 변수입니다.
         /// </summary>
         /// <remarks>
-        /// The identifier is used to reference a specific sub-graph within the system, enabling
-        /// interactions and transitions within the finite state machine framework.
+        /// 이 변수는 시스템 내 특정 서브 그래프를 참조하기 위해 사용됩니다.
+        /// 상태 전환 및 상태 머신 내 서브 그래프 간 상호작용을 지원하는 데 필수적입니다.
         /// </remarks>
         [SerializeField, DontCreateProperty]
         private UGUID _subGraphGuid;
 
 
         /// <summary>
-        /// Represents the type of the current state node in the finite state machine (FSM).
+        /// 상태 노드의 타입을 나타내는 추상 속성입니다.
         /// </summary>
         /// <remarks>
-        /// This property is used to distinguish the category of state nodes within a state machine,
-        /// such as regular action states, entry/exit points, or composite states that contain sub-graphs.
-        /// In the context of <see cref="SubGraphState"/>, this property always returns <see cref="StateNodeType.SubGraph"/>.
+        /// 이 속성은 상태 노드의 유형을 반환하며, 서브그래프 상태(SubGraphState)의 경우 항상 <see cref="StateNodeType.SubGraph"/>를 반환합니다.
         /// </remarks>
+        /// <returns>상태 노드의 타입을 나타내는 <see cref="StateNodeType"/> 값.</returns>
         public override StateNodeType nodeType
         {
             get { return StateNodeType.SubGraph; }
@@ -57,35 +62,27 @@ namespace TaskStreamer.FSM
 
 
         /// <summary>
-        /// Gets or sets the unique identifier (GUID) for the subgraph associated with this state.
+        /// 서브그래프의 고유 식별자를 나타내는 프로퍼티.
         /// </summary>
         /// <remarks>
-        /// The <c>subGraphGuid</c> property is used to identify and reference a specific subgraph within
-        /// the system. It is of type <see cref="UGUID"/>, which is a serializable Unity-specific GUID
-        /// implementation. This property is crucial for linking and managing subgraph-related functionality.
-        /// The subgraph identified by this GUID must exist and be retrievable during the lifecycle of the state.
-        /// During state awakening, the subgraph is fetched using this GUID. An assertion ensures that the
-        /// subgraph is valid and correctly associated with this state.
+        /// 이 프로퍼티는 서브그래프를 식별하기 위해 사용되는 <c>UGUID</c> 값을 저장 및 제공한다.
+        /// <see cref="TaskStreamer.FSM.SubGraphState"/> 클래스는 이 값을 기반으로 서브그래프를 관리 및 참조한다.
         /// </remarks>
         public UGUID subGraphGuid
         {
-            get { return _subGraphGuid; } 
-            
-            set { _subGraphGuid = value; } 
+            get { return _subGraphGuid; }
+
+            set { _subGraphGuid = value; }
         }
 
 
         /// <summary>
-        /// Represents the type of the sub-graph associated with the state.
+        /// 추상 상태 클래스에서 특정 하위 그래프의 유형(GraphType)을 나타내는 속성입니다.
         /// </summary>
         /// <remarks>
-        /// This property is used to define the type of the sub-graph (e.g., Behavior Tree, Finite State Machine, or Goal-Oriented Action Planning)
-        /// as specified in the <see cref="TaskStreamer.GraphType"/> enumeration.
-        /// It is implemented in derived classes to distinguish the specific sub-graph type associated with the state.
+        /// 이 속성은 하위 그래프의 종류를 정의하며, 해당 상태에서 동작하는 그래프의 유형을 식별하는 데 사용됩니다.
+        /// GraphType 열거형의 값을 반환하며, 예를 들어 Behavior Tree(BT), Finite State Machine(FSM), Goal Oriented Action Planning(GOAP) 등이 포함됩니다.
         /// </remarks>
-        /// <value>
-        /// A <see cref="TaskStreamer.GraphType"/> value that indicates the type of the sub-graph.
-        /// </value>
         public abstract GraphType subGraphType
         {
             get;
@@ -93,29 +90,29 @@ namespace TaskStreamer.FSM
 
 
         /// <summary>
-        /// Called when the state is awakened. This method is responsible for assigning
-        /// the subGraph by retrieving it from the graph asset using the subGraphGuid.
-        /// Ensures that the subGraph is properly initialized and asserts its existence
-        /// for error handling.
+        /// 상태가 활성화될 때 호출된다. 이 메서드는 subGraphGuid를 사용하여 그래프 에셋에서 서브 그래프를 가져와
+        /// _subGraph에 할당하며, 서브 그래프가 올바르게 초기화되었는지 확인하고 그 존재를 보증한다.
         /// </summary>
+        /// <exception cref="System.Diagnostics.DebugAssertException">
+        /// 서브 그래프가 Null인 경우 발생한다.
+        /// </exception>
         public override void OnAwake()
         {
-            _subGraph = streamer.graphAsset.GetGraph(subGraphGuid); 
+            _subGraph = streamer.graphAsset.GetGraph(subGraphGuid);
 
-            Debug.Assert(_subGraph != null, "SubGraph not found"); 
+            Debug.Assert(_subGraph != null, "SubGraph not found");
         }
 
 
         /// <summary>
-        /// Executes when the state is entered and ensures the associated sub-graph is properly initialized.
+        /// 상태가 활성화될 때 실행되며, 관련된 서브 그래프를 적절히 초기화한다.
         /// </summary>
         /// <remarks>
-        /// This method resets the sub-graph when the state is entered. If the sub-graph is null, an error message is logged.
-        /// Additionally, this method sets the blockTransition property to true, potentially preventing state transitions
-        /// until a specific condition is met or additional logic is executed.
+        /// 서브 그래프가 null인 경우 오류 메시지를 기록하고, 초기화가 이루어지지 않는다.
+        /// 서브 그래프 초기화와 함께 상태 전이 방지를 위한 blockTransition 속성이 true로 설정된다.
         /// </remarks>
         /// <exception cref="System.NullReferenceException">
-        /// Throws an exception if the sub-graph is not initialized before entering the state.
+        /// 서브 그래프가 활성화되기 전에 초기화되지 않은 경우 예외가 발생한다.
         /// </exception>
         protected override void OnEnter()
         {
@@ -128,17 +125,21 @@ namespace TaskStreamer.FSM
             this._subGraph.ResetGraph();
 
             //서브 그래프로가 실행 중일땐, 실행되다말고, 다른 노드로 이동되면 안되기 때문에 상태 전이를 막는다.
-            this.blockTransition = true;
+            this.blockTransition = !canTransitionDuringExecution.value;
         }
 
 
-        /// Handles the update logic specific to the state.
-        /// This method is called during the update cycle of the state machine.
-        /// If the `_subGraph` is null, it logs an error. Otherwise, it updates the graph
-        /// using the `UpdateGraph()` method. If the graph's status is not `Status.Running`,
-        /// the `blockTransition` property is set to false.
-        /// This method is used to control the internal behavior of a state's graph
-        /// and manage its transition blocking status dynamically.
+        /// <summary>
+        /// 상태가 업데이트될 때 호출되는 메서드.
+        /// 상태의 업데이트 주기 동안 논리 처리를 수행하며, 서브 그래프의 상태와 전이 여부를 관리한다.
+        /// </summary>
+        /// <remarks>
+        /// 서브 그래프가 null인 경우 오류를 로그로 출력한다. 서브 그래프의 상태 업데이트 결과가
+        /// <c>Status.Running</c>이 아닌 경우, 전이를 차단하지 않도록 <c>blockTransition</c>을 false로 설정한다.
+        /// </remarks>
+        /// <exception cref="System.NullReferenceException">
+        /// <c>_subGraph</c>가 null인 경우 발생.
+        /// </exception>
         protected override void OnUpdate()
         {
             if (_subGraph is null)
@@ -148,7 +149,7 @@ namespace TaskStreamer.FSM
             }
 
             //서브 그래프로가 Failure나 Success를 반환하면, 그래프 동작이 끝났다는 것이므로 상태 전이를 다시 허용한다.
-            if (_subGraph.UpdateGraph() != Status.Running)
+            if (_subGraph.UpdateGraph() != Status.Running || canTransitionDuringExecution.value)
             {
                 this.blockTransition = false;
             }
@@ -156,11 +157,11 @@ namespace TaskStreamer.FSM
 
 
         /// <summary>
-        /// Executes any necessary cleanup or termination logic when exiting the state.
+        /// 상태를 종료할 때 필요한 정리 또는 종료 로직을 실행한다.
         /// </summary>
         /// <remarks>
-        /// This method ensures that the associated subgraph is properly stopped during the exit process.
-        /// If the subgraph is null, an error message will be logged.
+        /// 이 메서드는 연결된 서브 그래프가 상태 종료 과정에서 적절히 중단되도록 보장한다.
+        /// 서브 그래프가 null인 경우 오류 메시지가 기록된다.
         /// </remarks>
         protected override void OnExit()
         {
@@ -170,7 +171,7 @@ namespace TaskStreamer.FSM
                 return;
             }
 
-            _subGraph.StopGraph();
+            this._subGraph.StopGraph();
         }
     }
 }
